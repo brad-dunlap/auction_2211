e227d48bb95de098def56a96933df3707f223905
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-08 10:52:21 -0500

    feat: tests for attributes

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,7 +3,14 @@ require './lib/item'
 RSpec.describe Item do
 	let(:item1){ Item.new('Chalkware Piggy Bank') }
 	let(:item2) { Item.new('Bamboo Picture Frame') }
-	it 'initializes' do
-		expect(item1). to be_instance_of(Item)
+	
+	describe '#initialize' do
+		it 'exists' do
+			expect(item1).to be_instance_of(Item)
+		end
+
+		it 'has attributes' do
+			expect(item1.name).to eq("Chalkware Piggy Bank")
+		end
 	end
 end
