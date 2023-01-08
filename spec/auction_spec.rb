a1b946640bcb96799c94ad49f0c1581c0308b1e4
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-08 11:10:34 -0500

    feat: add items to auction

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,11 +1,25 @@
 require './lib/auction'
 
 RSpec.describe Auction do
-	let(:auction) { Auction.new }
+	let(:auction){ Auction.new }
+	let(:item1){ Item.new('Chalkware Piggy Bank') }
+	let(:item2) { Item.new('Bamboo Picture Frame') }
 	
 	describe '#initialize' do
 		it 'exists' do
 			expect(auction).to be_instance_of(Auction)
+		end
+
+		it 'has attributes' do
+			expect(auction.items).to eq(nil)
+		end
+	end
+
+	describe '#add_items' do
+		it 'can add items to the auction' do
+			auction.add_item(item1)
+			auction.add_item(item2)
+			expect(auction.items).to eq([item1, item2])
 		end
 	end
 end
