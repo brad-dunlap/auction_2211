860b282435c9995e79bb2916ae63b86180d7169e
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-08 11:22:47 -0500

    feat: can add bid to an item

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,9 +5,15 @@ require './lib/item'
 require './lib/attendee'
 
 RSpec.describe Auction do
-	let(:auction){ Auction.new }
-	let(:item1){ Item.new('Chalkware Piggy Bank') }
+	let(:auction) { Auction.new }
+	let(:item1) { Item.new('Chalkware Piggy Bank') }
 	let(:item2) { Item.new('Bamboo Picture Frame') }
+	let(:item3) { Item.new('Homemade Chocolate Chip Cookies') }	
+	let(:item4) { Item.new('2 Days Dogsitting') }
+	let(:item5) { Item.new('Forever Stamps') }
+	let(:attendee1) { Attendee.new(name: 'Megan', budget: '$50') }
+	let(:attendee2) { Attendee.new(name: 'Bob', budget: '$75') }
+	let(:attendee3) { Attendee.new(name: 'Mike', budget: '$100') }
 	
 	describe '#initialize' do
 		it 'exists' do
@@ -32,6 +38,17 @@ RSpec.describe Auction do
 			auction.add_item(item1)
 			auction.add_item(item2)
 			expect(auction.item_names).to eq(['Chalkware Piggy Bank','Bamboo Picture Frame'])
+		end
+	end
+
+	describe 'bidding' do
+		it 'can add a bid' do
+			auction.add_item(item1)
+			auction.add_item(item2)
+			auction.add_item(item3)
+			auction.add_item(item4)
+			auction.add_item(item5)
+			expect(item1.bids).to eq({})
 		end
 	end
 end
